b1b8afb14cfd3d590cfdf197c9fb4e664301bb34
Alaa Radwan <alaa.a.radwan@gmail.com>
2023-10-18 19:25:06 -0700

    00

---
+++ 100-atoi.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _atoi - Convert a string to an integer.
+ * @s: The string to convert.
+ *
+ * Return: The converted integer.
+ */
+int _atoi(char *s)
+{
+int sign = 1; /* Positive by default */
+int result = 0;
+int i = 0;
+
+    /* Skip whitespace */
+while (s[i] == ' ')
+{
+i++;
+}
+
+    /* Check for sign */
+if (s[i] == '-' || s[i] == '+')
+{
+if (s[i] == '-')
+sign = -1;
+i++;
+}
+
+    /* Convert string to integer */
+while (s[i] >= '0' && s[i] <= '9')
+{
+result = result * 10 + (s[i] - '0');
+i++;
+}
+
+return (result *sign);
+}
+
